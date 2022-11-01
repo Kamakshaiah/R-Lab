bed775a2aa14c4eccb69b7e83c5366e3cc161e96
Kamakshaiah <dr.m.kamakshaiah@gmail.com>
2022-11-01 13:47:24 +0530

    updated crm script

--- course_work/projects/crm.R
+++ course_work/projects/crm.R
@@ -5,10 +5,11 @@ data_base <- data.frame(customers, pur_amt)
 # data_base
 
 add_transaction <- function(customer, amount){
-  return(rbind(data_base, c(customer, amount)))
+  data_base <<- rbind(data_base, c(customer, amount))
+  return(data_base)
 }
 
-add_transaction('Z', 100)
+add_transaction('Z', 231)
 
 add_customer <- function(customer){
   customers <- c(customers, customer)
@@ -17,7 +18,16 @@ add_customer <- function(customer){
   return(data_base)
 }
 
-# add_customer('Z')
+add_customer('Z')
+
+remove_customer <- function(customer){
+  data_base <<- data_base[-with(data_base, grep(customer, data_base[, 1])),]
+  return(data_base)
+}
+
+remove_customer('J')
+remove_customer('Z')
+# data_base
 
 # # finds 'A'
 # subset(data_base, data_base['customers']=='A')
@@ -26,20 +36,28 @@ add_customer <- function(customer){
 # cust_data['pur_amt'] <-  100
 # cust_data
 # 
-# data_base[with(data_base, customers == 'A'), ] # equal to subset
-# # excludes 'A'
-# data_base[!with(data_base, customers == 'A'), ]
+data_base[with(data_base, customers == 'A'), ] # equal to subset
+# excludes 'A'
+data_base[!with(data_base, customers == 'A'), ]
 
 add_amount <- function(customer, amount){
   if(customer %in% data_base[, 1]){
     cust_data <- subset(data_base, data_base['customers']==customer)
-    cust_data['pur_amt'] = cust_data['pur_amt'] + amount
+    if(is.na(cust_data['pur_amt'])) {
+      cust_data['pur_amt'] <- amount
+      
+    } else {
+      cust_data['pur_amt'] <- as.numeric(cust_data['pur_amt'][1, ]) + amount  
+    }
+    
     data_base <<- data_base[!with(data_base, customers == customer), ]
-    data_base <- rbind(data_base, cust_data)
+    data_base <<- rbind(data_base, cust_data)
+    return(data_base)
   }
 }
 
 add_amount('Z', 100)
+data_base
 
 calculate_credits <- function(cust, amount){
   credit <- matrix(NA, 10, 1)
@@ -52,11 +70,31 @@ calculate_credits <- function(cust, amount){
   return(credit)
 }
 
-credits <- mapply(transaction, customers, pur_amt)
+calc_credit <- function(customer){
+  if(customer %in% data_base[, 1]){
+    cust_data <- subset(data_base, data_base['customers'] == customer)
+    if (cust_data['pur_amt'][1, ] > 100 & cust_data['pur_amt'][1, ] < 200){
+      credit <- cust_data['pur_amt'][1, ] * 0.1
+      return(credit)
+    } else if (cust_data['pur_amt'][1, ] > 200 & cust_data['pur_amt'][1, ] < 300){
+      credit <- cust_data['pur_amt'][1, ] * 0.1
+      return(credit)
+    }
+  }
+  # return(credit)
+}
+
+print(calc_credit('A'))
+
+credits <- mapply(calc_credit, customers)
 
 # data.frame(customers, pur_amt, credits)
 
 cust_credit <- function(customer){
   cust_data <- subset(data_base, data_base['customers']==customer)
   return(cust_data['credits'])
+}
+
+for (i in 1:dim(data_set)[1]){
+  print(paste[data_set[i, 1], data_set[i, 2]])
 }
