e8102b6bc39c6939e5f38648c325590045f29e9d
Kamakshaiah <dr.m.kamakshaiah@gmail.com>
2023-08-17 13:55:37 +0530

    updated logical operator script

--- course_work/fundamentals/and-or-logic.R
+++ course_work/fundamentals/and-or-logic.R
@@ -41,3 +41,34 @@ end <- Sys.time()
 c(end-start) # Time difference of 7.950896 secs
 
 # conclusion - it is just about performance difference 
+
+
+# applications
+
+gender1 <- sample(c('male', 'female'), 10, replace = T)
+gender2 <- sample(c('male', 'female'), 10, replace = T)
+
+start <- Sys.time()
+for (i in 1:10){
+  if (gender1[i] == 'male' && gender2[i] == 'female'){
+    invisible(print('male-male scenario'))
+  } else {
+    invisible(print('not male-male scenario'))
+  }
+}
+end <- Sys.time()
+print(end-start)
+# Time difference of 0.006978035 secs
+
+start <- Sys.time()
+for (i in 1:10){
+  if (gender1[i] == 'male' & gender2[i] == 'female'){
+    invisible(print('male-male scenario'))
+  } else {
+    invisible(print('not male-male scenario'))
+  }
+}
+end <- Sys.time()
+print(end - start)
+
+# Time difference of 0.007652998 secs
