a02849587d82dd82ec9af12ffce107d372740ea2
Kamakshaiah <dr.m.kamakshaiah@gmail.com>
2023-10-03 11:52:22 +0530

    updated cortest

---
+++ course_work/fundamentals/cor-test-multi.R
@@ -0,0 +1,30 @@
+# correlation 
+x <- sample(1:10)
+y <- sample(1:10)
+
+cor(x, y) # pearson
+cor(x, y, method = 'spearman')
+cor(x, y, method = 'kendall')
+
+head(USArrests)
+cor(USArrests) # multivariate 
+
+plot(cor(USArrests)) # not appropriate
+heatmap(cor(USArrests))
+
+cor.test(USArrests) # not correct
+
+dim(USArrests)
+
+for (i in 1:dim(USArrests)[2]){
+  if (i == dim(USArrests)[2]){
+    ct <- cor.test(USArrests[, i], USArrests[, 1])
+    print(c(ct$statistic, ct$p.value))
+  } else {
+    ct <- cor.test(USArrests[, i], USArrests[, i+1])
+    print(c(ct$statistic, ct$p.value))  
+  }
+    
+  print(i)
+  
+}
